038bfa95f2b8b6489693eeead121fea376c2a468
Valentin Viovi <valentin.v3719@gmail.com>
2024-10-16 11:51:09 +0200

    ajout de l'installation de k3s dans le lancement des vm

--- p1/vvioviS.sh
+++ p1/vvioviS.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "coucou"
-
+curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --cluster-init
 
--- p1/vvioviSW.sh
+++ p1/vvioviSW.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 echo "coucouSW"
+curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - agent --server https://192.168.56.110:6443
