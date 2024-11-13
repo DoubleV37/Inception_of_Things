9f8b56968329ce0b770c1d1a7ed135356a9ef70b
jduval <jduval@student.42angouleme.fr>
2024-11-13 14:17:58 +0100

    [p2][ongoing] - Create and test all the apps 	- create app2 with 3 replicas 	- create app3 with a default selection

--- p2/apps/start.sh
+++ p2/apps/start.sh
@@ -1,9 +1,21 @@
 minikube start
 minikube addons enable ingress
-kubectl create configmap nginx-index --from-file=staticfiles/index.html
-kubectl create configmap nginx-static-content --from-file=staticfiles/dead.gif
-kubectl apply -f configfiles/deployment.yaml
-kubectl apply -f configfiles/service.yaml
+kubectl create configmap app1-index --from-file=app1/staticfiles/index.html
+kubectl create configmap app1-static-content --from-file=app1/staticfiles/dead.gif
+kubectl apply -f app1/deployment.yaml
+kubectl apply -f app1/service.yaml
+
+kubectl create configmap app2-index --from-file=app2/staticfiles/index.html
+kubectl create configmap app2-static-content --from-file=app2/staticfiles/burn.gif
+kubectl apply -f app2/deployment.yaml
+kubectl apply -f app2/service.yaml
+
+kubectl create configmap app3-index --from-file=app3/staticfiles/index.html
+kubectl create configmap app3-static-content --from-file=app3/staticfiles/crying.gif
+kubectl apply -f app3/deployment.yaml
+kubectl apply -f app3/service.yaml
+
 sleep 10
-kubectl apply -f configfiles/ingress.yaml
+
+kubectl apply -f ingress.yaml
 minikube ip
