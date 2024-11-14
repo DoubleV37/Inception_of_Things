9831dd56963ca0c377bdb567acfe00dfa96e3c70
Gazzopar <gazzopar@student.42angouleme.fr>
2024-11-14 14:40:45 +0100

    modification de la configuration du cluster k3s

--- p1/vvioviS.sh
+++ p1/vvioviS.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -sfL https://get.k3s.io | K3S_TOKEN=my-cluster-token sh -s - server --cluster-init --write-kubeconfig-mode 644 --node-ip 192.168.56.110
+curl -sfL https://get.k3s.io | K3S_TOKEN=my-cluster-token sh -s - server --write-kubeconfig-mode 644 --node-ip 192.168.56.110 --flannel-iface eth1
